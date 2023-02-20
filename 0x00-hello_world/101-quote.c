c33e9cbaffed7adc96534bd1a73b55177605f17e
Okechukwudivine <okechukwudivine54@gmail.com>
2023-02-20 11:15:07 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - wrinting a c program and print it.
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful"  - Dora Korpar, 2015 - 10 - 19\n");
+	return(1);
+}
